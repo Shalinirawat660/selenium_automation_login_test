304008394d00f96db4ff5a71a9b4106dad537c80
shalini <109856379+Shalinirawat660@users.noreply.github.com>
2023-09-13 09:29:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys  # Import to simulate key-driven events
+from selenium.webdriver.common.keys import Keys  #to simulate key-driven events
 
-# Initialize a Chrome WebDriver instance, providing the path to the ChromeDriver executable
+#a Chrome WebDriver instance,path to the ChromeDriver executable
 driver = webdriver.Chrome('C:\\Users\\shalini\\Downloads\\chromedriver-win64\\chromedriver')
 
-# Define the URL of the web application you want to automate (in this case, LeetCode login)
+#URL of the web application (in this case, LeetCode login)
 url = "https://leetcode.com/accounts/login/?next="
 
-# Open the specified URL in the Chrome browser
+# Opening the URL in the Chrome browser
 driver.get(url)
 
-# Locate the username and password input fields on the web page
+# locating the username and password input fields on the page
 username_field = driver.find_element_by_name("login")
 password_field = driver.find_element_by_name("password")
 
-# Simulate user input by entering a username and password
-username_field.send_keys("example@gmail.com")  # Replace with your actual email
-password_field.send_keys("examplepassword")    # Replace with your actual password
+# Simulating user input by entering username and password
+username_field.send_keys("example@gmail.com")
+password_field.send_keys("examplepassword")
 
-# Locate the login button on the web page and simulate a click event
+# Locating the login button on the web page to simulate a click event
 login_button = driver.find_element_by_id("signin_btn")
 login_button.click()
 
-# Close the browser and WebDriver session after completing the login process
+# Closing the browser and WebDriver after completing the login process
 driver.quit()
